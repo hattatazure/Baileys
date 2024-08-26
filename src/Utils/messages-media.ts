bfb4107d5da070dc6a631d3dabec34171b53482a
hattatazure <138884286+hattatazure@users.noreply.github.com>
2024-08-26 10:17:55 +0300

    Update messages-media.ts

--- src/Utils/messages-media.ts
+++ src/Utils/messages-media.ts
@@ -614,7 +614,17 @@ export const getWAUploadToServer = (
 
 			const auth = encodeURIComponent(uploadInfo.auth) // the auth token
 			const url = `https://${hostname}${MEDIA_PATH_MAP[mediaType]}/${fileEncSha256B64}?auth=${auth}&token=${fileEncSha256B64}`
+
+			const streamToBuffer = async (stream: Readable): Promise<Buffer> => {
+			    const chunks: Uint8Array[] = [];
+			    for await (const chunk of stream) {
+			        chunks.push(chunk);
+			    }
+			    return Buffer.concat(chunks);
+			};
 			
+			// Kullanım
+			const buffer = await streamToBuffer(stream);
 			let result: any
 			try {
 
